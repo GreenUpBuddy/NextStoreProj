0fa0386890bed31139d3cc7b251abf9d96ee646d
GreenUpBuddy <105259071+GreenUpBuddy@users.noreply.github.com>
2024-04-15 20:25:52 -0700

    Routing actually is fixed now

--- src/components/nav/drawer.js
+++ src/components/nav/drawer.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { Drawer, Box, Typography, IconButton, List, ListItem, ListItemIcon,ListItemText } from "@mui/material";
+import { Drawer, Box, Typography, IconButton, List, ListItemButton, ListItemIcon,ListItemText } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import InboxIcon from '@mui/icons-material/Inbox';
 import MailIcon from "@mui/icons-material/Mail";
@@ -19,12 +19,12 @@ export const MuiDrawer = () =>{
                 <Box p={2} width={250} textAlign= "center" role="presentation">
                 <List>
                     {["About", "Starred"].map((text, index) => (
-                    <ListItem key={text} component={Link} href={"/" + text.toLowerCase()} passHref>
+                    <ListItemButton key={text} component={Link} to={"/" + text.toLowerCase()}>
                         <ListItemIcon>
                         {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                         </ListItemIcon>
                         <ListItemText primary={text} />
-                    </ListItem>
+                    </ListItemButton>
                     ))}
                 </List>
                 </Box>
